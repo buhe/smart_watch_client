e466c916259712adf46508ec69e090a2816226cf
buhe <bugu1986@126.com>
2022-03-16 10:53:54 +0800

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import Navigation from './components/Navigation';
-import AuthProvider from './features/auth/AuthContext';
 import {theme} from './theme';
 
 const App = () => {
   return (
     <NativeBaseProvider theme={theme}>
-      <AuthProvider>
         <Navigation />
-      </AuthProvider>
     </NativeBaseProvider>
   );
 };
